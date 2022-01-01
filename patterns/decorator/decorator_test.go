1af5bac4700df8198c1817472f5aefe74b264eb3
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-01-01 00:44:09 +0000

    Addition of functional builder

--- patterns/decorator/decorator_test.go
+++ patterns/decorator/decorator_test.go
@@ -5,16 +5,14 @@ import (
 	"testing"
 )
 
-
-
 func TestTree_Decorate_should_have_size(t *testing.T) {
 	size := TreeSizeDecorator{Decorator: &Tree{"Fur tree"}, Size: "small"}
 	got := size.Decorate()
-	assert.Equal(t,"Fur tree small", got)
+	assert.Equal(t, "Fur tree small", got)
 }
 
 func TestTree_Decorate_should_have_colour_and_size(t *testing.T) {
 	colour := TreeColourDecorator{Decorator: &TreeSizeDecorator{Decorator: &Tree{"Fur tree"}, Size: "small"}, Decoration: "red"}
 	got := colour.Decorate()
-	assert.Equal(t,"Fur tree small red", got)
+	assert.Equal(t, "Fur tree small red", got)
 }
