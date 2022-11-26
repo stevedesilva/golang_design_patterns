93d320c60119c109978d2181694cb5d92ff3697c
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-11-26 23:45:20 +0000

    simple factory

---
+++ patterns/factory/creation.go
@@ -0,0 +1,20 @@
+package factory
+
+import "errors"
+
+type Person struct {
+	Name     string
+	Age      int
+	EyeCount int
+}
+
+func NewPerson(name string, age int) (*Person, error) {
+	if age > 120 {
+		return nil, errors.New("invalid age")
+	}
+	return &Person{
+		Name:     name,
+		Age:      age,
+		EyeCount: 2, // Default value uses function
+	}, nil
+}
